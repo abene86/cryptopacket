2ed0ee64eee2c76d63d1ecef21fe71b76d5f2607
Abenezer Gebeyehu <Abenezersoccer@gmail.com>
2022-01-06 15:44:14 -0500

    add the boxes

--- javascripts/back.js
+++ javascripts/back.js
@@ -1,23 +1,43 @@
+//hold the value for mainDiv that hold our content
 var mainDiv;
+//hold all the small div objects
+// we created through jquery
+var divObjects=[];
 
 var main=function(){
- var i;
+ var titles;
  mainDiv=$("<div>").addClass("holdMainContent");
- var titles=$("<h1>").text("CryptoPackets").addClass("maintitle");
+ titles=$("<h1>").text("CryptoPackets").addClass("maintitle");
  $("main").append(mainDiv);
  $(mainDiv).append(titles);
-
-//  for(i=0; i<5; i++){
-//     createBoxes();
-//  }
-
+ createBoxes(40);
+ populate();
 
 };
 $(document).ready(main);
-
-var createBoxes=function(){
-    var div=$("<div>").addClass("boxInside");
-    $(mainDiv).append(div);
-
-
+//creates the boxes
+var populate=function(){
+   var i;
+   for(i=0; i<divObjects.length; i++){
+        var image=$("<img>").addClass("iconImage");
+        var  ticker=$("<p>").addClass("tickerText");
+        image.attr("src", "https://cryptoicons.org/api/icon/eth/50");
+    //    divObjects[i].append(image);
+       ticker.text("eth");
+       divObjects[i].append(ticker);
+   }
+};
+var createBoxes=function(number){
+    var div1;
+    var div2;
+    var i;
+    for(i=0; i<number; i++){
+        if(i%8 === 0){
+          div1=$("<div>").addClass("boxOutside");
+          $(mainDiv).append(div1);
+        }
+        div2=$("<div>").addClass("boxInside");
+        divObjects[i]=(div2);
+        $(div1).append(div2);
+   }
 };
